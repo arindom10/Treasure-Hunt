4b3cf7ed90386c27b855c0285e00511e371734f4
arindom10 <karindom7@gmail.com>
2023-08-27 02:04:57 +0600

    project ready

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import HookProject from "./Projects/HookProject";
+import StudioProject from "./Projects/StudioProject";
+
+function App() {
+  return (
+    <div>
+      {/* <HomePage /> */}
+      <HookProject />
+    </div>
+  );
+}
+
+export default App;
